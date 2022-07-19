3b7dc8e080714ee98b74ba70eea1f14db198f535
Ehsan Ali <ehsanaliwalleem@gmail.com>
2022-07-18 19:52:23 -0700

    Created initial template.

---
+++ src/components/Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Heading 1</h1>
+      <p>This is a paragraph.</p>
+    </div>
+  );
+}
+export default Note;
